9aafb8f86a5b27cecfc2f8d950a3769c9a3602d1
John McIntyre <john@jumpingrivers.com>
2019-10-23 17:10:40 +0100

    Add Fun5.R

--- R/Fun5.R
+++ R/Fun5.R
@@ -1,3 +1,17 @@
+#' @title A function for testing whether the sum of two numbers is greater than 5
+#'
+#' @description A really good test function.
+#' Perhaps the best function ever!
+#'
+#' A work of pure genius.
+#' @param x a number
+#' @param y another number
+#' @return a number
+#' @export
+#' @examples
+#' subtract(5, 10)
+#' ## Can also use negative numbers
+#' subtract(-5, 10)
 #' @export
 Fun5 = function (x, y) {
   if (x + y > 5){
