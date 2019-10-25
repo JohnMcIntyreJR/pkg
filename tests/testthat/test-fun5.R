fb4d948e245cc67173537f31dbe4441aca1d8c0e
Jamie <jamie@jumpingrivers.com>
2019-10-25 11:37:14 +0100

    adding some example test and some description changes for john

---
+++ tests/testthat/test-fun5.R
@@ -0,0 +1,5 @@
+test_that('Fun 5 correctly identifies sums bigger than 5',{
+	expect_output(Fun5(3,3), "x + y is greater than 5", fixed = TRUE)
+	expect_output(Fun5(1,1), 'x + y is not greater than 5', fixed = TRUE)
+	expect_output(Fun5(2,3), 'x + y is not greater than 5', fixed = TRUE)
+})
